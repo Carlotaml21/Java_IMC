1fa8c47c0991725629d752badd64d40f6487a14d
Carlotaml21 <carlotaml21@gmail.com>
2025-02-09 20:15:09 +0100

    Change test

--- src/test/java/com/carlotaml21/IMCCalculatorTest.java
+++ src/test/java/com/carlotaml21/IMCCalculatorTest.java
@@ -1,10 +1,16 @@
 package com.carlotaml21;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class IMCCalculatorTest {
+
     @Test
     public void testCalculateIMC() {
-        assertEquals(22.86, IMCCalculator.calculateIMC(70, 1.75), 0.01);
+        IMCCalculator calculator = new IMCCalculator();
+        double result = calculator.calculateIMC(70, 1.75);
+
+        Assertions.assertEquals(22.85, result, 0.1);
     }
+}
+
